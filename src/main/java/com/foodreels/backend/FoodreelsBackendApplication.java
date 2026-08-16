1abbec4cfca55646d6f2babafad3861f86b4c2b2
Calwin Samuel <calwinsamuel0@gmail.com>
2026-08-16 11:33:21 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/foodreels/backend/FoodreelsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foodreels.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodreelsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodreelsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodreels/backend/FoodreelsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodreels.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodreelsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
