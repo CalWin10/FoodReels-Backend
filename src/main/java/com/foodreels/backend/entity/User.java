6890b8a617bc5d9d398398e8c74adf37b0a2261b
Calwin Samuel <calwinsamuel0@gmail.com>
2026-08-18 12:35:35 +0530

    feat: complete user entity

--- src/main/java/com/foodreels/backend/entity/User.java
+++ src/main/java/com/foodreels/backend/entity/User.java
@@ -1,8 +1,15 @@
 package com.foodreels.backend.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,5 +18,36 @@ public class User {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String profileImageUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void OnCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void OnUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+
 
 }
---
+++ src/main/java/com/foodreels/backend/entity/UserRole.java
@@ -0,0 +1,8 @@
+package com.foodreels.backend.entity;
+
+public enum UserRole {
+    USER,
+    RESTAURANT_OWNER,
+    ADMIN,
+
+}
