952fd0a4d2d459212f6a8e5b458d3634f01931c0
Calwin Samuel <calwinsamuel0@gmail.com>
2026-08-18 12:12:19 +0530

    feat: add user entity

---
+++ src/main/java/com/foodreels/backend/entity/User.java
@@ -0,0 +1,15 @@
+package com.foodreels.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+}
